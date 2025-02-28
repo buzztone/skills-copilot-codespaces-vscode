c704edb9ab1d29d1d28107712788bab88cbfe1f6
Neil Murray <neilmrr75@gmail.com>
2025-02-28 06:10:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillmember() {
+    var memberData = {
+        name: "Jung",
+        age: 23,
+        hobby: ["soccer", "basketball", "swimming"],
+                email: "jung@example.com"
+            };
+        }
+        
